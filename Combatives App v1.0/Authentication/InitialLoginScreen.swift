fcfc03d3705d73c2fc8c87801fcfaff79ed0652d
ebleakney <C24Ethan.Bleakney@afacademy.af.edu>
2024-01-18 10:05:00 -0700

    Authentication Screen

---
+++ Combatives App v1.0/Authentication/InitialLoginScreen.swift
@@ -0,0 +1,18 @@
+//
+//  AuthenticationView.swift
+//  Combatives App v1.0
+//
+//  Created by Ethan Bleakney on 1/18/24.
+//
+
+import SwiftUI
+
+struct AuthenticationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AuthenticationView()
+}
