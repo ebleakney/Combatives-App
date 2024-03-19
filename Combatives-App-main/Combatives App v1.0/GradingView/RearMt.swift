b9e70bde83f1df9aedd991b67db44ab270d96602
JahWayne <c24james.huber@afacademy.af.edu>
2024-03-19 11:12:15 -0600

    added a timer. It is broken as of now

--- Combatives-App-main/Combatives App v1.0/GradingView/RearMt.swift
+++ Combatives-App-main/Combatives App v1.0/GradingView/RearMt.swift
@@ -4,80 +4,93 @@ struct ScoringItem: Identifiable {
     let id = UUID()
     var description: String
     var points: Int
+    var isExtraPoints: Bool
+    var isSelected: Bool = false
 }
 
 struct RearMountView: View {
     @State private var dpScoringItems: [ScoringItem] = [
-        ScoringItem(description: "Lost position or established closed or 1/2 guard (top/bottom)", points: 0),
-        ScoringItem(description: "Established front, knee, or side mount", points: 3),
-        ScoringItem(description: "Recovered rear mount", points: 5),
-        ScoringItem(description: "Maintained", points: 7),
-        ScoringItem(description: "Rear Naked Choke (RNC)", points: 10),
-        ScoringItem(description: "Guillotine", points: 10)
+        ScoringItem(description: "Lost position or established closed or 1/2 guard (top/bottom)", points: 0, isExtraPoints: false),
+        ScoringItem(description: "Established front, knee, or side mount", points: 3, isExtraPoints: false),
+        ScoringItem(description: "Recovered rear mount", points: 5, isExtraPoints: false),
+        ScoringItem(description: "Maintained", points: 7, isExtraPoints: false),
+        ScoringItem(description: "Rear Naked Choke (RNC)", points: 10, isExtraPoints: false),
+        ScoringItem(description: "Guillotine (extra points)", points: 10, isExtraPoints: true)
     ]
     
     @State private var ndpScoringItems: [ScoringItem] = [
-        ScoringItem(description: "Got choked", points: 0),
-        ScoringItem(description: "No escape", points: 3),
-        ScoringItem(description: "Escape to any other NDP or 1/2 guard (top/bottom)", points: 5),
-        ScoringItem(description: "Escape to neutral or establish closed guard DP", points: 7),
-        ScoringItem(description: "Escape to top dominant", points: 10),
-        ScoringItem(description: "RNC or Guillotine", points: 10)
+        ScoringItem(description: "Got choked", points: 0, isExtraPoints: false),
+        ScoringItem(description: "No escape", points: 3, isExtraPoints: false),
+        ScoringItem(description: "Escape to any other NDP or 1/2 guard (top/bottom)", points: 5, isExtraPoints: false),
+        ScoringItem(description: "Escape to neutral or establish closed guard DP", points: 7, isExtraPoints: false),
+        ScoringItem(description: "Escape to top dominant", points: 10, isExtraPoints: false),
+        ScoringItem(description: "RNC or Guillotine (extra points)", points: 10, isExtraPoints: true)
     ]
     
-    @State private var selectedDpIndex: Int?
-    @State private var selectedNdpIndex: Int?
-    
+    @State private var timerIsRunning = false
+    @State private var timeElapsed: TimeInterval = 0
+    private let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+
     var body: some View {
         VStack {
+            Text(timerText)
+                .font(.title)
+                .padding()
+                .onReceive(timer) { _ in
+                    if self.timerIsRunning {
+                        self.timeElapsed += 1
+                    }
+                }
+            
             List {
                 Section(header: Text("Rear Mt (DP)").foregroundColor(.black).padding().background(Color.blue)) {
-                    ForEach(Array(dpScoringItems.enumerated()), id: \.element.id) { index, item in
-                        ScoringRow(item: item, isSelected: self.selectedDpIndex == index)
-                            .onTapGesture {
-                                self.selectedDpIndex = self.selectedDpIndex == index ? nil : index
-                            }
+                    ForEach(dpScoringItems.indices, id: \.self) { index in
+                        ScoringRow(item: self.$dpScoringItems[index])
                     }
                 }
                 Section(header: Text("Rear Mt (NDP)").foregroundColor(.black).padding().background(Color.gray)) {
-                    ForEach(Array(ndpScoringItems.enumerated()), id: \.element.id) { index, item in
-                        ScoringRow(item: item, isSelected: self.selectedNdpIndex == index)
-                            .onTapGesture {
-                                if index == self.ndpScoringItems.count - 1 && self.selectedNdpIndex == index {
-                                    return // Don't deselect if it's the last item
-                                }
-                                self.selectedNdpIndex = self.selectedNdpIndex == index ? nil : index
-                            }
+                    ForEach(ndpScoringItems.indices, id: \.self) { index in
+                        ScoringRow(item: self.$ndpScoringItems[index])
                     }
                 }
             }
             .listStyle(GroupedListStyle())
             .background(Color(.systemGroupedBackground)) // Matches the typical background color for grouped lists
+            
+            HStack {
+                Spacer()
+                Button(action: {
+                    self.timerIsRunning.toggle()
+                }) {
+                    Text(timerIsRunning ? "Stop Timer" : "Start Timer")
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(timerIsRunning ? Color.red : Color.green)
+                        .cornerRadius(8)
+                }
+                .padding(.horizontal)
+                
+                Button(action: {
+                    self.timerIsRunning = false
+                    self.timeElapsed = 0
+                }) {
+                    Text("Reset Timer")
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color.blue)
+                        .cornerRadius(8)
+                }
+                .padding(.horizontal)
+                Spacer()
+            }
+            .padding(.vertical)
         }
     }
-}
 
-struct ScoringRow: View {
-    var item: ScoringItem
-    var isSelected: Bool
-    
-    var body: some View {
-        HStack {
-            Text(item.description)
-                .foregroundColor(.black)
-            Spacer()
-            Text("\(item.points)")
-                .foregroundColor(isSelected ? .white : .black)
-                .padding()
-                .background(isSelected ? Color.blue : Color.gray)
-                .clipShape(Circle())
-                .overlay(
-                    Circle().stroke(Color.black, lineWidth: isSelected ? 0 : 1)
-                )
-        }
-        .padding(.vertical, 4)
-        .background(isSelected ? Color.blue : Color.clear)
-        .cornerRadius(8)
+    private var timerText: String {
+        let minutes = Int(timeElapsed) / 60
+        let seconds = Int(timeElapsed) % 60
+        return String(format: "%02d:%02d", minutes, seconds)
     }
 }
 
--- Combatives-App-main/Combatives App v1.0/GradingView/ScoringRow.swift
+++ Combatives-App-main/Combatives App v1.0/GradingView/ScoringRow.swift
@@ -1,8 +1,29 @@
-//
-//  ScoringRow.swift
-//  Combatives App v1.0
-//
-//  Created by James Huber on 3/12/24.
-//
+import SwiftUI
 
-import Foundation
+struct ScoringRow: View {
+    @Binding var item: ScoringItem // Use Binding to make item mutable
+    
+    var body: some View {
+        HStack {
+            Text(item.description)
+                .foregroundColor(.black)
+            Spacer()
+            Text("\(item.points)")
+                .foregroundColor(item.isSelected ? .white : .black)
+                .padding()
+                .background(item.isSelected ? Color.blue : Color.gray)
+                .clipShape(Circle())
+                .overlay(
+                    Circle().stroke(Color.black, lineWidth: item.isSelected ? 0 : 1)
+                )
+        }
+        .padding(.vertical, 4)
+        .background(item.isSelected ? Color.blue : Color.clear)
+        .cornerRadius(8)
+        .onTapGesture {
+            if item.isExtraPoints || item.isSelected {
+                item.isSelected.toggle()
+            } /// Make changes so there is 1 case to handle whenever an item is selcted. It will check to make sure it is the highest points if it is the highest points it willl stay selected. If an extra points option is selected
+        }
+    }
+}
