5dba02e019728b1f7ea2c4456f0ebd2ae2bd8c57
JahWayne <c24james.huber@afacademy.af.edu>
2024-04-05 10:39:25 -0600

    lesson 28 initial

--- Combatives-App-main/Combatives App v1.0/Visuals/StudentListView/GradeListView/AddGradeView.swift
+++ Combatives-App-main/Combatives App v1.0/Visuals/StudentListView/GradeListView/AddGradeView.swift
@@ -1,38 +1,50 @@
-//
-//  AddGradeView.swift
-//  Combatives App v1.0
-//
-//  Created by James Huber on 2/27/24.
-//
-
 import SwiftUI
 
 struct AddGradeView: View {
     @Binding var isPresented: Bool
     @Binding var newGrade: String
-    @State private var selectedGradeType = "Standing GR"
+    @State private var selectedGradeType: String? = nil
+    @State private var shouldNavigate: Bool = false // Added to control navigation programmatically
     
     var body: some View {
-        VStack {
-            Picker("Grade Type", selection: $selectedGradeType) {
-                Text("Standing GR").tag("Standing GR")
-                Text("Ground GR").tag("Ground GR")
-            }
-            .pickerStyle(SegmentedPickerStyle())
-            .padding()
-            
-            TextField("Enter grade", text: $newGrade)
+        NavigationStack {
+            VStack {
+                Picker("Grade Type", selection: $selectedGradeType) {
+                    Text("Standing GR").tag("Standing GR")
+                    Text("Ground GR").tag("Ground GR")
+                }
+                .pickerStyle(SegmentedPickerStyle())
                 .padding()
-            
-            Button("Add") {
-                // Add logic to save the new grade
-                isPresented = false
+                
+                TextField("Enter grade", text: $newGrade)
+                    .padding()
+                
+                NavigationLink(destination: ROEView(), isActive: $shouldNavigate) {
+                    EmptyView()
+                }
+                /*navigationDestination(isPresented: $shouldNavigate) {
+                    ROEView()
+                }*/
+                
+                Button("Add") {
+                    // Add logic to save the new grade
+                    if selectedGradeType == "Ground GR" {
+                        shouldNavigate = true
+                        isPresented = false
+                    }
+                }
+                .padding()
+                
+                Spacer()
             }
             .padding()
-            
-            Spacer()
+            .navigationTitle("Add Grade")
         }
-        .padding()
-        .navigationTitle("Add Grade")
+    }
+}
+
+struct AddGradeView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddGradeView(isPresented: .constant(false), newGrade: .constant(""))
     }
 }
